fd423e1f701823c911ed627bd63833713bb69f17
Jason Hernandez <kaltre10@gmail.com>
2023-01-29 19:28:47 -0500

    test cors2

--- server/middlewares/cors.js
+++ server/middlewares/cors.js
@@ -1,9 +1,9 @@
 function cors(req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET, POST, PUT, PATCH, DELETE');
+    // res.setHeader('Access-Control-Allow-Origin', '*');
+    // res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET, POST, PUT, PATCH, DELETE');
   
-    res.setHeader('Access-Control-Allow-Headers',  
-     'Content-Type, Authorization');
+    // res.setHeader('Access-Control-Allow-Headers',  
+    //  'Content-Type, Authorization');
   
     next();
 }
