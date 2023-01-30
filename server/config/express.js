e2d31777275c30760c430ac177d667e483e7665a
Jason Hernandez <kaltre10@gmail.com>
2023-01-29 19:20:36 -0500

    config cors

--- server/config/express.js
+++ server/config/express.js
@@ -4,7 +4,8 @@ const auth = require('../middlewares/auth');
 const cors = require('cors');
 module.exports = (app) => {
     
-    app.use(cors({origin: 'http://localhost:5001', credentials: true}));
+    // app.use(cors({origin: 'http://localhost:5001', credentials: true}));
+    app.use(cors());
     app.use(express.json({ limit: '50mb' }));
     app.use(express.urlencoded({ limit: '50mb', extended: true }));
     app.use(cookieParser());
